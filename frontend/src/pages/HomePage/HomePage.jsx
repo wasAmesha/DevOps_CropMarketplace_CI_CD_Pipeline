64234c05b748e0ad0444d5864d22f4e365c592a5
wasAmesha <ameshawahalathanthri20@gmail.com>
2024-06-29 15:01:16 +0530

    update commit

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -16,7 +16,7 @@ function HomePage() {
       <img src={"/Navbar/walll.jpg"} alt="" className="crop" />
       <div className="type-writer-container">
         <TypeWriter
-          text="Welcome to CropXchange Digital arketplace"
+          text="Welcome to CropXchange Digital Marketplace"
           textStyle={{
             fontFamily: "Gill Sans",
             fontSize: "20px",
